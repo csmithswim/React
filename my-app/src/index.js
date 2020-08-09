e9a67055f68fb2727e7a703b8d25ffd34e4ed9f2
Cullen Smith <cullensmi@gmail.com>
2020-08-09 11:53:35 -0400

    added calculate winner function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,84 +3,111 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-      return (
-        <button 
-          className="square" 
-          onClick={props.onClick()}         
-        >                     
-          {props.value}
-        </button>
-      );
-}   
-
-  class Board extends React.Component {  
-    constructor(props) {        /*Method that creates/initializes an object created with a class, limited to one per class, can  */
-      super(props);             /* super keyword to call the constructor of the super class, a subclass of the parent */
-      this.state = { 
-        squares: Array(9).fill(null),    /* Setting Board's initial state to contain an array of 9 nulls corresponding to 9 squares */
-      };
-    }
-
-    handleClick(i) {
-      const squares = this.state.squares.slice(); //Creating a copy of the squares array to modify, immutability is ipmortant!
-      squares[i] = 'X';
-      this.setState({squares: squares});
-    }
-
-    renderSquare(i) {
-      return (
-      <Square 
-      value={this.state.squares[i]}          /*Passing a prop from the parent Board component to the Square component */
-      onClick={() => this.handleClick(i)}
-      />       
-      );
-  }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
+}
+
+class Board extends React.Component {
+  constructor(props) { /*Method that creates/initializes an object created with a class, limited to one per class, can  */
+    super(props); /* super keyword to call the constructor of the super class, a subclass of the parent */
+    this.state = {
+      squares: Array(9).fill(null), /* Setting Board's initial state to contain an array of 9 nulls corresponding to 9 squares */
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice(); //Creating a copy of the squares array to modify, immutability is ipmortant!
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}  /*Passing a prop from the parent Board component to the Square component */
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }      
+    
+        return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
