7751a60923330b1a07493a4102093e7d460224c7
Cullen Smith <cullensmi@gmail.com>
2020-08-13 12:58:20 -0400

    added more exercises

--- reactFCC.js
+++ reactFCC.js
@@ -550,7 +550,7 @@ input: event.target.value
   }
 };
 
-//
+//You can create a controlled form that the state controls in React
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
@@ -587,7 +587,7 @@ class MyForm extends React.Component {
   }
 };
 
-//
+//You can pass state as props to child components
 class MyApp extends React.Component {
   constructor(props) {
     super(props);
@@ -622,7 +622,7 @@ class Navbar extends React.Component {
   }
 }
 
-//
+//You can pass a callback functio as props
 
 class MyApp extends React.Component {
   constructor(props) {
@@ -630,7 +630,7 @@ class MyApp extends React.Component {
     this.state = {
       inputValue: ''
     }
-    this.handleChange = this.handleChange.bind(this);
+  this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
     this.setState({
@@ -640,9 +640,11 @@ class MyApp extends React.Component {
   render() {
     return (
        <div>
-        { /* change code below this line */ }
-
-        { /* change code above this line */ }
+         <GetInput
+           input={this.state.inputValue}
+           handleChange={this.handleChange}/>
+         <RenderInput
+           input={this.state.inputValue}/>
        </div>
     );
   }
@@ -678,7 +680,147 @@ class RenderInput extends React.Component {
   }
 };
 
+//Lifecycle methods perform actions at specific points in the lifecycle of a component, componentWillMount() is called before render()
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    console.log('hello world')
+  }
+  render() {
+    return <div />
+  }
+};
 
+//componentDidMount() handles most API calls or calls to the server, it's called after a component is mounted to the DOM
+//This example uses setTimeout to mimic an API call
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null
+    };
+  }
+  componentDidMount() {
+    setTimeout( () => {
+      this.setState({
+        activeUsers: 1273
+      });
+    }, 3000);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: { this.state.activeUsers}</h1>
+      </div>
+    );
+  }
+};
+//You can directly add event listeners to the dom using the lifecycle methods, React uses a synthetic dom and you typically need to add functional event listeners using this practice
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+    this.handleEnter = this.handleEnter.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+  // change code below this line
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+  // change code above this line
+  handleEnter() {
+    this.setState({
+      message: this.state.message + "You pressed the enter key! "
+    });
+  }
+  handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      this.handleEnter();
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    );
+  }
+}
+
+//You can tell React when to rerender when props change, in this instance we want it to rerender when the value of a certain prop is even
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+     // change code below this line
+    if(nextProps.value % 2 == 0){
+    return true}
+    return false;
+     // change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>
+  }
+};
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState({
+      value: this.state.value + 1
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value}/>
+      </div>
+    );
+  }
+};
+//You can apply styling to JSX elements by enclosing the style = {{}} in objects and making sure that properties must be camelCase
+class Colorful extends React.Component {
+  render() {
+    return (
+      <div style={{color: "red", fontSize: 72}}> Big Red</div>
+    );
+  }
+};
+//You can state styles to be an object with properties and then set the values later in a JSX element
+const styles = {
+  color: 'purple',
+  fontSize: 40,
+  border: "2px solid purple",
+};
+
+class Colorful extends React.Component {
+  render() {
+    // change code below this line
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    // change code above this line
+  }
+};
 
 
 
